9bd3b5584e429342151a66e3c58c296a13b05ee0
Uwe Stahl <UweStahl@Uwes-MacBook-Pro.local>
2013-08-07 20:57:52 +0200

    Initial Commit

---
+++ CoreDataAudioIPhone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreDataAudioIPhone
+//
+//  Created by Uwe Stahl on 07.08.13.
+//  Copyright (c) 2013 Uwe Stahl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreDataAudioIPhone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreDataAudioIPhone
+//
+//  Created by Uwe Stahl on 07.08.13.
+//  Copyright (c) 2013 Uwe Stahl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
